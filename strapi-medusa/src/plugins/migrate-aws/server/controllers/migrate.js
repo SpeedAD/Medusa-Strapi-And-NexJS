cafdf99c2c322121ebca1d3f6f38f93a055ec084
@agam.dubey <agam.dubey@schbang.com>
2024-06-01 14:53:04 +0530

    remove() keys

--- strapi-medusa/src/plugins/migrate-aws/server/controllers/migrate.js
+++ strapi-medusa/src/plugins/migrate-aws/server/controllers/migrate.js
@@ -1,10 +1,6 @@
 "use strict";
 var AWS = require("aws-sdk");
 var async = require("async");
-// S3_ACCESS_KEY_ID=AKIASCOG7JVY4KFXUF5A
-// S3_SECRET_ACCESS_KEY=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
-// S3_REGION=ap-south-1
-// S3_BUCKET=medusa-static-assets
 
 module.exports = ({ strapi }) => ({
   async index(ctx) {
